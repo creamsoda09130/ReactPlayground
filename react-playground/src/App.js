077e658c2bf1335950574769e9fd45edcfedea57
mk-0929 <moeka.okubo@gmail.com>
2023-10-05 19:50:27 +0900

    if文利用

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,5 +1,39 @@
 import './App.css';
 
+function OddButton() {
+  const img = {
+    alt: 'Reactのロゴ',
+    src: 'logo512.png',
+  };
+  return (
+    <button className="button button--red">
+      <span className="text">数値が奇数だった場合に表示されるボタン</span>
+      <img
+        className="img"
+        src={img.src}
+        alt={img.alt}
+      />
+    </button>
+  );
+}
+
+function EvenButton() {
+  const img = {
+    alt: 'Reactのロゴ',
+    src: 'logo512.png',
+  };
+  return (
+    <button className="button button--blue">
+      <span className="text">数値が偶数だった場合に表示されるボタン</span>
+      <img
+        className="img"
+        src={img.src}
+        alt={img.alt}
+      />
+    </button>
+  );
+}
+
 function MyButton() {
   const img = {
     alt: 'Reactのロゴ',
@@ -17,14 +51,34 @@ function MyButton() {
   );
 }
 
+function Message() {
+  const message = 'というわけで奇数でした';
+  return (
+    <p>{message}</p>
+  );
+}
+
 function App() {
   const title = 'Welcome to My App';
+  const number = Math.floor(Math.random() * 100);
+  let button;
+  if (number % 2 === 1) {
+    button = <OddButton />;
+  } else {
+    button = <EvenButton />;
+  }
   return (
     <div className="App">
       <h1 className="title">{title + '←ここまでが変数'}</h1>
       {/* TASK: ここにclassNameを付与することはできない、同じコンポーネントで違うクラスを付けたい時の書き方調査 */}
       <div className="contents">
-        <MyButton></MyButton>
+        <p>{number}</p>
+        {button}
+        {number % 2 === 1 ? (
+          <Message />
+        ) : (
+          <p>[ここは何も記載したくない]</p>
+        )}
       </div>
     </div>
   );
