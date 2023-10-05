8750270920ca1353f7b78ab854743308a77856b9
mk-0929 <moeka.okubo@gmail.com>
2023-10-05 15:24:43 +0900

    ヘッダー部分の削除

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function MyButton() {
@@ -12,21 +11,8 @@ function MyButton() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div>
+        <h1>Welcome to My App</h1>
         <MyButton></MyButton>
       </div>
     </div>
